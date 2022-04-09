bbd0d5a47757dcd20ecb64ae0a7dcaaa0a1b9620
edwardlanto <edwardlanto.developer@gmail.com>
2022-04-08 21:24:56 -0700

    removed example json

--- cypress/integration/3-home/home.spec.js
+++ cypress/integration/3-home/home.spec.js
@@ -4,6 +4,7 @@ beforeEach(() => {
 
 describe('Main components loading', () => {
     it('It should load external image', () => {
+      
       // Find an external image
       cy.get('img').should('exist');
     });
@@ -18,17 +19,11 @@ describe('It should be setting input state', () => {
 });
 
 describe('It should show results', () => {
-  it('It should load external image', () => {
-
+  it('It should have minimum 5 results', () => {
     cy.get('[data-cy="search__input"]')
     .type('giraffes').should('have.value', 'giraffes');
     cy.get('[data-cy="search__submit"]').click();
     cy.url().should('include', 'search');
     cy.get('[data-cy="search__result"]').its('length').should('be.gte', 5);
   });
-
-  // it('It should have a valid input', () => {
-  //   cy.get('[data-cy="search__input"]')
-  //   .type('giraffes').should('have.value', 'giraffes')
-  // });
 });
